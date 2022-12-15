f8d15eb66cdad9b279430fc8665c7e22deb04670
EMANUEL2552 <emanuel.fernandesdasilva@gmail.com>
2022-12-15 13:31:45 -0300

    spring-leo-calc

--- src/main/java/br/com/cod3r/meuprojeto/controllers/ClienteController.java
+++ src/main/java/br/com/cod3r/meuprojeto/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.meuprojeto.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,20 @@ import models.Cliente;
 @RequestMapping("/clientes")
 public class ClienteController {
 	
-	@GetMapping(path = "/qualquer")
+	@GetMapping()
 	public Cliente obterCliente() {
 		return new Cliente(28,"pedro","123.456.789-00" );
 	}
 
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "maria", "987.654.321-00");
+	}
 }
+
+
+
+
+
+
